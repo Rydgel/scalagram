ac63a734d181f654d3c6cea1fa483ebdc88c9918
Jérôme Mahuet <jerome.mahuet@gmail.com>
2014-12-04 13:33:36 +0100

    basic tests

---
+++ src/test/scala/com/rydgel/scalagram/ScalagramSpec.scala
@@ -0,0 +1,45 @@
+package com.rydgel.scalagram
+
+import com.rydgel.scalagram.responses._
+import org.scalatest._
+import org.scalatest.matchers.{BePropertyMatchResult, BePropertyMatcher}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.io.Source
+import scala.util.Try
+
+class ScalagramSpec extends FlatSpec with Matchers {
+
+  private def anInstanceOf[T](implicit tag : reflect.ClassTag[T]) = {
+    val clazz = tag.runtimeClass.asInstanceOf[Class[T]]
+    new BePropertyMatcher[AnyRef] { def apply(left: AnyRef) =
+      BePropertyMatchResult(left.getClass.isAssignableFrom(clazz), "an instance of " + clazz.getName) }
+  }
+
+  // todo a better error message to explain people to create the token.txt file
+  private def readAccessTokenFromConfig(): AccessToken = {
+    Try {
+      val tokenFile = Source.fromURL(getClass.getResource("/token.txt")).mkString
+      AccessToken(tokenFile.split("=").toList(1))
+    }.getOrElse(throw new Exception(
+      "Please provide a valid access_token by making a token.txt in resources.\n" +
+      "See token.txt.default for detail."
+    ))
+  }
+
+  val auth = readAccessTokenFromConfig()
+  val wrongToken = AccessToken("this is a bullshit access token")
+
+  "A failed request" should "return a failed promise" in {
+    an [Exception] should be thrownBy Await.result(Scalagram.userInfo(wrongToken, "36783"), 10 seconds)
+  }
+
+  "userInfo" should "return a Response[Profile]" in {
+    val request = Await.result(Scalagram.userInfo(auth, "36783"), 10 seconds)
+    request should be (anInstanceOf[Response[Profile]])
+    request.data.get.id should be ("36783")
+  }
+
+}
